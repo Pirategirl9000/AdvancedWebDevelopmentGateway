26da55fa222948df9b2edfcd9f29453e876ffa3a
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-04-15 15:03:12 -0500

    Added smartwatch project

---
+++ Smartwatch-FAQ/README.md
@@ -0,0 +1,57 @@
+# Smartwatch-FAQ
+
+![Video of output](assets/outputVideo.gif)
+
+
+### Authors
+### :purple_heart: [Violet French](https://github.com/Pirategirl9000)<br>
+### :potted_plant: [Sarah Fenton](https://github.com/sarahfenton204)
+
+### Table of Contents
+* [Authors](#authors)
+* [Output](#camera-output)
+* [Purpose](#rotating_light-purpose)
+* [New Concepts](#triangular_flag_on_post-new-concepts)
+* [Script Breakdown](#newspaper-script-breakdown)
+* [Credits](#thumbsup-credits)
+
+### :rotating_light: Purpose
+### This program displays an FAQ page for a smartwatch that uses collapsible panels for questions. When the question is clicked an answer appears and a corresponding image is displayed at the top
+
+### :triangular_flag_on_post: New Concepts
+* Adding/Removing classes from DOM elements
+* Checking for and grabbing custom HTML attributes from DOM elements
+* Grabbing next elements based on sibling relationship of DOM elements
+* Checking equality of DOM elements
+* Finding the target of an `Event` object
+* Toggling the class of a DOM element
+* Mutation of an array using `Array.forEach()`
+
+### :newspaper: Script Breakdown
+#### :earth_americas: Globals
+* `faqImage`
+  * The image element we will replace on question clicks
+* `faqImageOrigSrc`
+  * The original image upon program startup
+  * Used when both questions are closed to reset the `faqImage` src
+* `faqImageOrigAlt`
+  * The alt text of the original image on program startup
+  * Used when both questions are closed to reset the `faqImage` alt text
+* `h2s`
+  * All the h2 elements in the HTML which store the questions
+  * Used to determine what header element was clicked so we can update the image and div element associated with the answer
+
+#### :construction_worker: Functions and Listeners
+* `toggleVisibility`
+  * Called by all header elements on click
+  * Updates the header element by: toggling the visibilty of the answer, closing all other answer divs, and updating the `faqImage`
+* `document.onDOMContentLoaded`
+  * Adds an onClick listener to all `h2s` elements that calls `toggleVisibility`
+
+### :camera: Output
+![Output of program](assets/output1.png)
+![Output of program](assets/output2.png)
+![Output of program](assets/output3.png)
+
+### :thumbsup: Credits
+###### This script is an adaptation of a script provided by [Debbie Johnson](https://github.com/dejohns2)
---
+++ Smartwatch-FAQ/faqs.js
@@ -0,0 +1,89 @@
+/**
+ * This program displays frequently asked questions about a watch
+ * with drop down menus that manipulate the dom images
+ * @author Violet French - https://github.com/Pirategirl9000
+ * @author Sarah Fenton - https://github.com/sarahfenton204
+ * @link https://github.com/Pirategirl9000/Smartwatch-FAQ
+ */
+
+/**
+ * The image DOM element that corresponds with each collapsible question
+ * @type {Element}
+ */
+const faqImage = document.querySelector("#faq_image");
+
+/**
+ * The original source image for the faq image
+ */
+const faqImageOrigSrc = faqImage.src;
+
+/**
+ * The original alt text for the faq image
+ */
+const faqImageOrigAlt = faqImage.alt;
+
+/**
+ * All the header 2 elements that are used for displaying the answers to questions
+ * @type {NodeListOf<Element>}
+ */
+const h2s = document.querySelectorAll("#faqs h2");
+
+/**
+ * Replaces the image and displays the answer to the question clicked
+ * @param evt the question that was clicked
+ */
+const toggleVisibility = evt => {
+    const ct = evt.currentTarget;
+
+    // Stores if all the collapsible are closed
+    let allClosed = false;
+
+    for (let heading of h2s) {
+
+        // If this is the heading that was clicked
+        if (heading === ct) {
+
+            if (heading.classList.contains("minus")) {
+                allClosed = true;
+            }
+
+            // Toggles the minus background image for the side of this question
+            heading.classList.toggle("minus");
+
+            // Displays the answer to the question clicked
+            heading.nextElementSibling.classList.toggle("open");
+
+            // Check if the header has the necessary attributes for updating the image
+            if (!heading.hasAttribute("data-img") || !heading.hasAttribute("data-alt")) return;
+
+            // Update the image
+            faqImage.src = heading.getAttribute("data-img");
+            faqImage.alt = heading.getAttribute("data-alt");
+        } else {
+            // Revert back to the plus symbol
+            heading.classList.remove("minus");
+
+            // Close the answer div element
+            heading.nextElementSibling.classList.remove("open");
+
+
+        }
+    }
+
+    if (allClosed) {
+        faqImage.src = faqImageOrigSrc;
+        faqImage.alt = faqImageOrigAlt;
+    }
+
+    evt.preventDefault();
+}
+
+
+/**
+ * Add click listeners to the headers
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    h2s.forEach(header => {
+        header.addEventListener("click", toggleVisibility);
+    })
+})
